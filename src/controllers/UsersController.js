8c086206403860f72f5b2a4eec2a95052b73306c
Luuwei <gislaine.jessica@hotmail.com>
2024-02-28 17:17:32 -0300

    Finalização do projeto

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,77 +1,98 @@
-const { hash, compare } = require("bcryptjs");
-const AppError = require("../utils/AppError");
+const { hash, compare } = require('bcryptjs')
 //conexão com banco de dados
-const sqliteConnetion = require("../database/sqlite");
+const sqliteConnetion = require('../database/sqlite')
+
+const AppError = require('../utils/AppError')
 
 class UsersController {
-    async create(request, response){
-        const { name, email, password } = request.body;
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-        const database = await sqliteConnetion();
-        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const database = await sqliteConnetion()
+    const checkUserExist = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
 
-        //verifica se o email está sendo utilizado por alguém 
-        if(checkUserExist){
-            throw new AppError("Este e-mail já está em uso.")
-        }
+    //Regex para validar o e-mail fornecido no formato de email
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-        const hashedPassword = await hash(password, 8)
-
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
-
-        return response.status(201).json();
-
+    //Verificação do Regex do padrão de email
+    if (!emailPattern.test(email)) {
+      throw new AppError('Por favor, insira um endereço de e-mail válido.')
     }
 
-    async update(request, response){
-        const { name, email, password, old_password } = request.body;
-        const user_id  = request.user.id;
+    //verifica se o email está sendo utilizado por alguém
+    if (checkUserExist) {
+      throw new AppError('Este e-mail já está em uso.')
+    }
 
-        const database = await sqliteConnetion();
-        const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+    const hashedPassword = await hash(password, 8)
 
-        if(!user) {
-            throw new AppError("Usuario não encontrado");
-        }
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
 
-        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    return response.status(201).json()
+  }
 
-        //verificação: se encontrar um email e se esse email for diferente do usuario, ta tentando mudar o email pra um email existente.
-        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
-            throw new AppError("Esse e-mail já está em uso.")
-        }
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const user_id = request.user.id
 
-        user.name = name ?? user.name; 
-        user.email = email ?? user.email;
-        //caso tenha nome ele adiciona, caso n informado ele mantem o que já está salvo, isso é o mesmo para o email.
-        
+    const database = await sqliteConnetion()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [
+      user_id
+    ])
 
-        if (password && !old_password){
-            throw new AppError("Você precisa informar a senha antiga para definir a nova senha.")
-        }
+    if (!user) {
+      throw new AppError('Usuario não encontrado')
+    }
 
-        if (password && old_password){
-            const checkOldPassword = await compare(old_password, user.password);
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
 
-            if(!checkOldPassword){
-                throw new AppError("A senha antiga não confere.")
-            }
+    //verificação: se encontrar um email e se esse email for diferente do usuario, ta tentando mudar o email pra um email existente.
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Esse e-mail já está em uso.')
+    }
 
-            user.password = await hash(password, 8);
-        }
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+    //caso tenha nome ele adiciona, caso n informado ele mantem o que já está salvo, isso é o mesmo para o email.
 
-        await database.run(`
+    if (password && !old_password) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para definir a nova senha.'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere.')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      `
             UPDATE users SET
             name = ?,
             email = ?,
             password = ?,
             updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, user.password, user_id]
-        )
+      [user.name, user.email, user.password, user_id]
+    )
 
-        return response.json();
-    }
+    return response.json()
+  }
 }
 
-module.exports = UsersController;
+module.exports = UsersController
